b362f1b75c0c23eeedaaa6b5be9b797890385037
phr3nzy <adilosama47@gmail.com>
2019-09-22 19:26:52 +0200

    Add: elevation to main sheet component

--- components/MainSheet.vue
+++ components/MainSheet.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="row">
-    <v-sheet class="my-10 mx-5 pa-5" color="primary" width="100%" dark>
+    <v-sheet
+      class="my-10 mx-5 pa-5"
+      color="primary"
+      width="100%"
+      elevation="6"
+      dark
+    >
       <span class="font-weight-thin display-2">Dashboard</span>
     </v-sheet>
   </div>

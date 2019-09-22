9a7d89276c923f7b4b3aba370b1127cbf3e553af
phr3nzy <adilosama47@gmail.com>
2019-09-22 18:11:58 +0200

    Add: Heroku Procfile

---
+++ pages/inventory.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs12 md4>
+      <h1>Inventory Page</h1>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  layout: ({ isMobile }) => (isMobile ? 'mobile' : 'default')
+}
+</script>

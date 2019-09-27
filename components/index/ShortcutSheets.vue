aa038780f99c44d1e6bddf4df94d3b7116564be6
phr3nzy <adilosama47@gmail.com>
2019-09-27 11:21:43 +0200

    Add: notifications shortcut

--- components/index/ShortcutSheets.vue
+++ components/index/ShortcutSheets.vue
@@ -42,16 +42,36 @@
         ></v-sheet
       >
     </v-col>
+    <v-col cols="12" sm="6" md="4" lg="3">
+      <v-sheet class="main-gradient pa-5" dark>
+        <v-row align="center" justify="center">
+          <v-card color="transparent" flat>
+            <v-icon size="160">{{ mdiBell }}</v-icon>
+            <v-card-actions class="text-center">
+              <v-btn :disabled="true" block class="black--text" color="white"
+                >0 Notifications</v-btn
+              >
+            </v-card-actions>
+          </v-card></v-row
+        ></v-sheet
+      >
+    </v-col>
   </v-row>
 </template>
 
 <script>
-import { mdiReceipt, mdiBottleTonicPlus, mdiFileChartOutline } from '@mdi/js'
+import {
+  mdiReceipt,
+  mdiBottleTonicPlus,
+  mdiFileChartOutline,
+  mdiBell
+} from '@mdi/js'
 export default {
   data: () => ({
     mdiReceipt,
     mdiBottleTonicPlus,
-    mdiFileChartOutline
+    mdiFileChartOutline,
+    mdiBell
   })
 }
 </script>

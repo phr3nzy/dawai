e3d70dacfab28e337b2ca9d48692143b9b445ce3
phr3nzy <adilosama47@gmail.com>
2019-10-24 12:56:56 +0200

    Add: search component autocomplete and loading methods

--- components/index/Search.vue
+++ components/index/Search.vue
@@ -1,30 +1,37 @@
 <template>
-  <v-row align="center" justify="center" dense>
-    <v-col cols="12" xs="12" md="8" lg="6">
+  <v-row align="center" justify="center">
+    <v-col cols="12" xs="12" md="8" lg="5">
       <v-autocomplete
-        v-model="selected"
-        label="Search for Medicine"
+        v-model="select"
         color="primary"
-        :items="medicine"
-        :filter="activeFilter"
-        :append-icon="mdiChevronDown"
-        :prepend-inner-icon="mdiCloudSearch"
-        outlined
-        rounded
+        label="Search for Medicine"
+        hint="Try 'Panadol'..."
+        :items="items"
+        :loading="loading"
+        :search-input.sync="search"
+        :prepend-inner-icon="mdiPill"
+        :clear-icon="mdiClose"
+        loader-height="3"
+        hide-details
+        hide-no-data
+        cache-items
+        clearable
+        autofocus
+        solo
       ></v-autocomplete>
-      <v-btn-toggle v-model="model" mandatory shaped>
-        <v-btn>
-          Exact
-          <v-icon right>{{ mdiBullseyeArrow }}</v-icon>
-        </v-btn>
-
-        <v-btn>
-          Loose
-          <v-icon right>{{ mdiFormatListBulletedType }}</v-icon>
-        </v-btn>
-      </v-btn-toggle>
     </v-col>
-    <v-col v-if="selected" class="mt-12" cols="12" xs="12">
+    <v-col class="text-center" cols="12" xs="12" md="12" lg="1">
+      <v-btn
+        color="primary"
+        :loading="isLoading"
+        :disabled="!select"
+        large
+        icon
+        @click="searchMedicine"
+        ><v-icon>{{ mdiMagnify }}</v-icon></v-btn
+      >
+    </v-col>
+    <v-col v-if="isLoading" class="mt-12" cols="12" xs="12">
       <v-sheet color="transparent" class="px-3 pt-3 pb-3">
         <v-skeleton-loader
           class="slight-br my-3"
@@ -53,7 +60,9 @@
 
 <script>
 import {
-  mdiCloudSearch,
+  mdiPill,
+  mdiMagnify,
+  mdiClose,
   mdiChevronDown,
   mdiBullseyeArrow,
   mdiFormatListBulletedType
@@ -62,12 +71,17 @@ export default {
   inject: ['theme'],
   data() {
     return {
-      selected: null,
-      mdiCloudSearch,
+      select: null,
+      search: '',
+      loading: false,
+      isLoading: false,
+      items: [],
+      mdiPill,
+      mdiMagnify,
+      mdiClose,
       mdiChevronDown,
       mdiBullseyeArrow,
       mdiFormatListBulletedType,
-      model: null,
       medicine: [
         'Panadol',
         'Panadol Extra',
@@ -76,27 +90,29 @@ export default {
         'Ibuprofen Extra',
         'Paracetamol',
         'Super Paracetamol'
-      ],
-      filters: [
-        {
-          value: 0,
-          fn: (item, queryText, itemText) => item.includes(queryText),
-          text: 'Exact Match'
-        },
-        {
-          value: 1,
-          fn: (item, queryText, itemText) =>
-            item.toLowerCase().includes(queryText),
-          text: 'Loose Match'
-        }
       ]
     }
   },
-
-  computed: {
-    activeFilter() {
-      if (this.model == null) return undefined
-      return this.filters[this.model].fn
+  watch: {
+    search(val) {
+      val && val !== this.select && this.querySelections(val)
+    }
+  },
+  methods: {
+    searchMedicine() {
+      this.isLoading = true
+      setTimeout(() => {
+        this.isLoading = false
+      }, 5000)
+    },
+    querySelections(v) {
+      this.loading = true
+      setTimeout(() => {
+        this.items = this.medicine.filter((e) => {
+          return (e || '').toLowerCase().includes((v || '').toLowerCase())
+        })
+        this.loading = false
+      }, 1000)
     }
   }
 }
